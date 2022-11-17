d68a8de396f727a812c0618e17df682a4e286275
Andy Jiang <andyjiangart@gmail.com>
2022-11-17 12:00:11 -0500

    added dropdown

--- src/Client/ClientHome.js
+++ src/Client/ClientHome.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { FormBusinessCode } from "./FormBusinessCode"
 import { ClientAppointments } from "./ClientAppointments"
 
@@ -5,12 +6,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 export const ClientHome = ({setPage, setBusiness, setUser, user, setLastAppointment }) => {
+    const [dropDown, setDropDown] = useState(false)
     return(
     <div>
         <header>
             <h1>Point<span>Mint</span></h1>
-            <button>
+            <button
+                onClick={e => {
+                    e.preventDefault()
+                    setDropDown(!dropDown)
+                }} 
+            >
                 <FontAwesomeIcon icon={faUser} />
+                {dropDown && 
+                    <div>
+                        <p>Profile</p>
+                        <p onClick={() => window.location.reload()}>Logout</p>
+                    </div>
+                } 
             </button>
         </header>
         <FormBusinessCode setPage={setPage} setBusiness={setBusiness} />
